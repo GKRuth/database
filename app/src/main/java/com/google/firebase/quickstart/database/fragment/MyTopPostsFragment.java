3b4a55732c865de8915a2dceb44f182906f17a12
GKRuth <kopanko.goha@yandex.ru>
2019-11-07 13:04:26 +0300

    Initial commit

---
+++ app/src/main/java/com/google/firebase/quickstart/database/fragment/MyTopPostsFragment.java
@@ -0,0 +1,21 @@
+package com.google.firebase.quickstart.database.fragment;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+public class MyTopPostsFragment extends PostListFragment {
+
+    public MyTopPostsFragment() {}
+
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        // [START my_top_posts_query]
+        // Мои посты отмечены звездами
+        String myUserId = getUid();
+        Query myTopPostsQuery = databaseReference.child("user-posts").child(myUserId)
+                .orderByChild("starCount");
+        // [END my_top_posts_query]
+
+        return myTopPostsQuery;
+    }
+}
